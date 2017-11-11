9d912a95ad0570d5894a27f5eb5aa8e754c3f058
Ashleigh Ryan <aryan34@gatech.edu>
2017-11-11 10:52:42 -0500

    Updated TestingTest

--- RatTracker/app/src/test/java/doperatz/rattracker/Model/TestingTest.java
+++ RatTracker/app/src/test/java/doperatz/rattracker/Model/TestingTest.java
@@ -2,8 +2,29 @@ package doperatz.rattracker.Model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.rules.ExpectedException;
+
 public class TestingTest {
 
-    //Paige's JUnit Test
-    
+    public static final int TIMEOUT = 200;
+    Model model = Model.getInstance();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    //Paige's JUnit Tests for checkPassword
+
+    public void testCheckPasswordException() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("User cannot be null.");
+        model.checkPassword(null);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testCheckPassword() {
+        
+    }
 }
