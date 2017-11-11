720cdd6a0504a6457cda6c8ede46e32724a57645
Ashleigh Ryan <aryan34@gatech.edu>
2017-11-11 11:30:18 -0500

    Added JUnits for Paige and Josh

--- RatTracker/app/src/test/java/doperatz/rattracker/Model/TestingTest.java
+++ RatTracker/app/src/test/java/doperatz/rattracker/Model/TestingTest.java
@@ -12,10 +12,9 @@ public class TestingTest {
     public static final int TIMEOUT = 200;
     Model model = Model.getInstance();
 
+    //Paige's JUnit Tests for checkPassword
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
-    //Paige's JUnit Tests for checkPassword
 
     public void testCheckPasswordException() {
         thrown.expect(IllegalArgumentException.class);
@@ -25,6 +24,28 @@ public class TestingTest {
 
     @Test(timeout = TIMEOUT)
     public void testCheckPassword() {
-        
+        User u1 = new User("Paige", "password", true);
+        User u2 = new User("Paige", "notPassword", true);
+        model.addUser(u1);
+
+        assertTrue(model.checkPassword(u1));
+        assertFalse(model.checkPassword(u2));
+    }
+
+    //Josh's JUnit Tests for isUser
+    public void testIsUserException() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("User cannot be null.");
+        model.isUser(null);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testIsUser() {
+        User there = new User("Josh", "myPass", false);
+        User notThere = new User("Bob", "hjkauf", false);
+        model.addUser(there);
+
+        assertTrue(model.isUser(there));
+        assertFalse(model.isUser(notThere));
     }
 }
